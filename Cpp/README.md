49844d597b423f3a88cb3c4208e77133b20de56f
ycq <ycq__ycq@live.com>
2021-01-31 12:33:15 +0800

    add java and cpp project

---
+++ Cpp/README.md
@@ -0,0 +1,1 @@
+This is a cpp project by CLion.
---
+++ Java/src/main/java/com/yangchunqiao/App.java
@@ -0,0 +1,13 @@
+package com.yangchunqiao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
